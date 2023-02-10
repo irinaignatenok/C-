69ea2b4da4336ada11cadf2bc7449d5d5a3555a3
Irina Ignatenok <specterhauz@gmail.com>
2023-02-10 14:57:00 +0200

    conditions

---
+++ Example005_conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine(" Hi");
+}
+else 
+{
+    Console.Write("Hello");
+    Console.WriteLine(username);
+}
